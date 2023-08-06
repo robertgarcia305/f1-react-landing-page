f45ee927bf05cb5bb15c5fb50bcbdc4d0f32a644
Robert Garcia <robertserranogar@gmail.com>
2023-08-06 16:06:42 -0600

    new commit

---
+++ src/Cards.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Cards = () => {
+    return (
+        <div>
+            <p>hello there</p>
+        </div>
+    );
+}
+
+export default Cards;
